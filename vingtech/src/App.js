b7ef48ccc61f19bab0f159e72bd20588ff79aee5
Vijaykumar Ingle <43980104+vijaykumaringle@users.noreply.github.com>
2021-07-19 01:04:42 +0530

    commit 2 - home page adjust
    commit 2 - home page adjust

--- vingtech/src/App.js
+++ vingtech/src/App.js
@@ -10,7 +10,7 @@ function App() {
           <div className="logo-div">
             <h2>
               <span style={{ fontWeight: 300 }}>ViNG</span>
-              <span style={{ fontWeight: 500,  textDecoration: 'overline'}}>
+              <span style={{ fontWeight: 500, textDecoration: "overline" }}>
                 Tech
               </span>
             </h2>
@@ -21,23 +21,23 @@ function App() {
             </p>
           </div>
         </div>
-        <div class="options">
-          <a href="/" class="options-link link-active">
+        <div className="options">
+          <a href="/" className="options-link link-active">
             Home
           </a>
-          <a href="/" class="options-link">
+          <a href="/" className="options-link">
             About
           </a>
-          <a href="/" class="options-link">
+          <a href="/" className="options-link">
             AstroViNG
           </a>
-          <a href="/" class="options-link">
+          <a href="/" className="options-link">
             Blog
           </a>
-          <a href="/" class="options-link">
+          <a href="/" className="options-link">
             Social Media
           </a>
-          <a href="/" class="options-link">
+          <a href="/" className="options-link">
             Contant Us
           </a>
         </div>
@@ -65,27 +65,77 @@ function App() {
         </div>
       </nav>
       <main>
-        <h1>Main</h1>
-        <p>
-          Vestibulum consectetur sit amet nisi ut consectetur. Praesent
-          efficitur, nibh vitae fringilla scelerisque, est neque faucibus quam,
-          in iaculis purus libero eget mauris. Curabitur et luctus sapien, ac
-          gravida orci. Aliquam erat volutpat. In hac habitasse platea dictumst.
-          Aenean commodo, arcu a commodo efficitur, libero dolor mollis turpis,
-          non posuere orci leo eget enim. Curabitur sit amet elementum orci,
-          pulvinar dignissim urna. Morbi id ex eu ex congue laoreet. Aenean
-          tincidunt dolor justo, semper pretium libero luctus nec. Ut vulputate
-          metus accumsan leo imperdiet tincidunt. Phasellus nec rutrum dolor.
-          Cras imperdiet sollicitudin arcu, id interdum nibh fermentum in.
-        </p>
-        <a
-          className="App-link"
-          href="https://vingtech.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More
-        </a>
+        
+        <section>
+          <h2>The Title</h2>
+          <p>
+            Some Text goes here, some text goes here, some text goes here, some
+            text goes here.
+          </p>
+          <a href="/" className="info-link">
+            Learn more...
+          </a>
+        </section>
+        <section>
+          <h2>The Title</h2>
+          <p>
+            Some Text goes here, some text goes here, some text goes here, some
+            text goes here.
+          </p>
+          <img
+            className="section-img"
+            src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/emoji-one/104/chart-with-upwards-trend_1f4c8.png"
+            alt="important graph"
+          />
+          <a href="/" className="info-link">
+            Learn more...
+          </a>
+        </section>
+        <section>
+          <h2>The Title</h2>
+          <img
+            alt="imgq1"
+            className="section-img profile"
+            src="http://placekitten.com/260/260"
+          />
+          <p>
+            Some Text goes here, some text goes here, some text goes here, some
+            text goes here.
+          </p>
+          <a href="/" className="info-link">
+            Learn more...
+          </a>
+        </section>
+        <section className="bigbottom">
+          <h2>The Title</h2>
+          <p>
+            Some Text goes here, some text goes here, some text goes here, some
+            text goes here.
+          </p>
+          <a className="bigbottom-link info-link" href="/">
+            Learn more...
+          </a>
+        </section>
+        <section className="bigtitle">
+          <h2 className="bigtitle-title">The Title</h2>
+          <p>
+            Some Text goes here, some text goes here, some text goes here, some
+            text goes here.
+          </p>
+          <a href="/" className="info-link">
+            Learn more...
+          </a>
+        </section>
+        <section>
+          <h2>The Title</h2>
+          <p>
+            Some Text goes here, some text goes here, some text goes here, some
+            text goes here.
+          </p>
+          <a href="/" className="info-link">
+            Learn more...
+          </a>
+        </section>
       </main>
       <aside>Related links</aside>
       <footer>Footer</footer>
